31dc0e542739703bc342965a0aad1050c0d0496f
Shuaibing <36749963+RysBen@users.noreply.github.com>
2020-07-03 19:16:56 +0800

    Update and rename FASTQ to fastq.py

---
+++ fastq.py
@@ -0,0 +1,13 @@
+       
+#qc
+def trim_base():
+    while qual[]
+
+
+def main():
+    with open('test.fq') as f:
+        while True:
+            try:
+                head,seq,sep,qual=f.next(),f.next(),f.next(),f.next()
+            except:
+                break
