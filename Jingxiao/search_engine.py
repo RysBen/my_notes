4f4ab98bd7bc56c2271aaea05dcaf330a1ede4d9
Shuaibing <36749963+RysBen@users.noreply.github.com>
2021-01-09 14:50:59 +0800

    Update search_engine.py

--- Jingxiao/search_engine.py
+++ Jingxiao/search_engine.py
@@ -35,3 +35,41 @@ class SimpleSearch(SearchEngineBase):
             if query in v:
                 result.append(k)
         return result
+
+#######################################################
+# 2. BagofWord
+#######################################################
+import re
+
+class BOWengine(SimpleSearch):
+    def __init__(self):
+        self.__name2text={}
+    def process_corpus(self,name,text):
+        #self.__name2text[name]=word_bag(text)   ## false
+        self.__name2text[name]=self.word_bag(text)
+    def search(self,query):
+        results=[]
+        #query=word_bag(query)   ##
+        query=self.word_bag(query)
+        for k,v in self.__name2text.items():
+            #if word_match(query,v):
+            if self.word_match(query,v):
+                results.append(k)
+        return results
+    
+    @staticmethod
+    def word_bag(text):
+            tmp=re.sub(r'([^\w]|\s+)',' ',text)
+            tmp=tmp.lower()
+            words=tmp.split(' ')
+            #words=filter(None,words)
+            return set(words)
+    @staticmethod
+    def word_match(query,bow):
+        results=[]
+        for q in query:
+            if q in bow:
+                return True
+            return False
+
+#
